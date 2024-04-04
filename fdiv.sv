f9e3d57ea53ab6c9c321f9f452a20dc51de88d28
Chasen12 <129551157+Chasen12@users.noreply.github.com>
2024-04-04 11:49:22 -0500

    //updated

--- fdiv.sv
+++ fdiv.sv
@@ -5,13 +5,16 @@ module fdiv(input  logic [31:0] N, D,
 			input  logic clk ,
             output logic [31:0] Q);
 
-logic ia [30:0];
+logic ia [29:0];
 logic A  [59:0];
 logic B  [59:0];
 logic regc [59:0];
 logic rega [59:0];
 logic regb [59:0];
 logic output [29:0];
+
+logic Nf [29:0];
+logic Df [29:0];
 
 mux2 muxA (ia, regc, op, A);
 mux4 muxB (N, D, rega, regb, rm, B);
@@ -20,8 +23,15 @@ flopren RC (clk, reset, enc, output, regc);
 flopren RA (clk, reset, ena, output, rega);
 flopren RB (clk, reset, enb, output, regb);
 
-//unpackage the data
 
+//unpack data
+
+assign Nf = 0'b00000000000000000000000000000 + N;
+assign Df = 0'b00000000000000000000000000000 + D;
+
+
+// gets us our signed bit 
+assign Q = N[31] ^ D[31]
 
 
 
