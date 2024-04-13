9e81256c972f5a85c6bbcb66ea6430e7971f848b
Chasen12 <129551157+Chasen12@users.noreply.github.com>
2024-04-13 13:13:06 -0500

    updated for testing

--- 0419-flopenr.sv
+++ 0419-flopenr.sv
@@ -4,8 +4,8 @@
 module flopren(input  logic       clk,
                input  logic       reset,
                input  logic       en, 
-               input  logic [3:0] d, 
-               output logic [3:0] q);
+               input  logic [29:0] d, 
+               output logic [29:0] q);
 
   // asynchronous reset 
   always_ff @(posedge clk, posedge reset)
--- fdiv.sv
+++ fdiv.sv
@@ -1,4 +1,4 @@
-module fdiv(input  logic [31:0] N, D,
+module fdiv (input  logic [31:0] N, D,
 			input  logic [1:0] rm,
 			input  logic op,
 			input  logic reset,
@@ -8,9 +8,9 @@ module fdiv(input  logic [31:0] N, D,
 logic ia [29:0];
 logic A  [29:0];
 logic B  [29:0];
-logic regc [59:0];
-logic rega [59:0];
-logic regb [59:0];
+logic regc [29:0];
+logic rega [29:0];
+logic regb [29:0];
 logic output [59:0];
 logic output2s [59:0];
 logic Nf [29:0];
@@ -20,26 +20,26 @@ logic Df [29:0];
 
 assign Nf = 0'b000000100000000000000000000000 + N[22:0];
 assign Df = 0'b000000100000000000000000000000 + D[22:0];
-assign ia = 0'b000000100000000000000000000000
+assign ia = 0'b000000110000000000000000000000;
 
 // gets us our signed bit 
-assign Q = N[31] ^ D[31]
+assign Q = N[31] ^ D[31];
 
 
 
 
-mux2 muxA (ia, regc[59:30], op, A);
-mux4 muxB (Nf, Df, rega[59:30], regb[59:30], rm, B);
+mux2 muxA (ia, regc, op, A);
+mux4 muxB (Nf, Df, rega, regb, rm, B);
 
-flopren RC (clk, reset, enc, output, regc);
-flopren RA (clk, reset, ena, output2s, rega);
-flopren RB (clk, reset, enb, output, regb);
+flopren RC (clk, reset, enc, output [59:30], regc);
+flopren RA (clk, reset, ena, output2s [59:30], rega);
+flopren RB (clk, reset, enb, output [59:30], regb);
 
 
 assign output = A * B;
 // need to work out two complment logic
 
-assign output2s  = ~output + 0'b1;
+assign output2s  = ~output;
   
   
 endmodule
