7924859db8aaaec326d5bc9fb068f2d4a4bfa150
Chasen12 <129551157+Chasen12@users.noreply.github.com>
2024-04-13 12:17:47 -0500

    updated

--- fdiv.sv
+++ fdiv.sv
@@ -6,29 +6,21 @@ module fdiv(input  logic [31:0] N, D,
             output logic [31:0] Q);
 
 logic ia [29:0];
-logic A  [59:0];
-logic B  [59:0];
+logic A  [29:0];
+logic B  [29:0];
 logic regc [59:0];
 logic rega [59:0];
 logic regb [59:0];
-logic output [29:0];
-
+logic output [59:0];
+logic output2s [59:0];
 logic Nf [29:0];
 logic Df [29:0];
 
-mux2 muxA (ia, regc, op, A);
-mux4 muxB (N, D, rega, regb, rm, B);
+//unpack N and D data to 30 bit var
 
-flopren RC (clk, reset, enc, output, regc);
-flopren RA (clk, reset, ena, output, rega);
-flopren RB (clk, reset, enb, output, regb);
-
-
-//unpack data
-
-assign Nf = 0'b00000000000000000000000000000 + N;
-assign Df = 0'b00000000000000000000000000000 + D;
-
+assign Nf = 0'b000000100000000000000000000000 + N[22:0];
+assign Df = 0'b000000100000000000000000000000 + D[22:0];
+assign ia = 0'b000000100000000000000000000000
 
 // gets us our signed bit 
 assign Q = N[31] ^ D[31]
@@ -36,18 +28,18 @@ assign Q = N[31] ^ D[31]
 
 
 
+mux2 muxA (ia, regc[59:30], op, A);
+mux4 muxB (Nf, Df, rega[59:30], regb[59:30], rm, B);
 
+flopren RC (clk, reset, enc, output, regc);
+flopren RA (clk, reset, ena, output2s, rega);
+flopren RB (clk, reset, enb, output, regb);
 
 
 assign output = A * B;
+// need to work out two complment logic
 
-  
-  
-  
-  
-
-  
-  
+assign output2s  = ~output + 0'b1;
   
   
 endmodule
